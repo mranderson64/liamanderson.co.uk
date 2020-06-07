45750f88e525459ffb499e70f88b4a9f493741ce
MrAnderson64 <mranderson64@live.com>
2020-06-07 05:47:14 +0100

    first iteration of the base CMS
    I should have made earlier commits but where too lazy

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+include 'urlParse.php';
+include 'head.php';
+echo $parseOutput;
+include 'foot.php';
+?>
---
+++ urlParse.php
@@ -0,0 +1,23 @@
+<?php 
+#$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\'); //uncomment before deployment dipshit
+$urip = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$uri = str_replace("liam", "", $urip); // liams stupid dev enviroment
+if($uri == "" || $uri == "/"){
+	$uri = "home";
+}
+$xml = simplexml_load_file("./XML/" . $uri . ".xml") or die("Error: Cannot create object");
+$title = $xml->title;
+$description = $xml->description;
+$useTemplate = $xml->useTemplate;
+$template = $xml->template;
+$usehtml = $xml->usehtml;
+$html = $xml->html;
+$contentTitle = $xml->contentTitle;
+$content = $xml->content;
+include "./templates/" . $template . ".php";
+if($useTemplate == true){
+	$parseOutput = $templateOutput;
+}else if($usehtml == true){
+	$parseOutput = $content;
+}
+?>
