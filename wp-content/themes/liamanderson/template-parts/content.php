5dea1cbdab205529aabd7c79a2ed4d6ae9a7111c
mranderson64 <mranderson64@live.com>
2020-08-09 22:58:40 +0100

    yes?

--- wp-content/themes/liamanderson/template-parts/content.php
+++ wp-content/themes/liamanderson/template-parts/content.php
@@ -36,13 +36,14 @@
 			); */
 			if ( is_search() || ! is_singular() && 'summary' === get_theme_mod( 'blog_content', 'full' ) ) {
 				the_excerpt();
-			} else {
-				the_content( __( 'Continue reading', 'liamanderson' ) );
 				if(strpos(strval($categorieC), 'snippets')){
 					echo '<div class="code"><pre rel="'.$categorieC[1]->name.'" class="'.$categorieC[1]->name.'"><code>';
 					get_field( "code" );
 					echo '</code></pre></div>';
 				}
+			} else {
+				the_content( __( 'Continue reading', 'liamanderson' ) );
+				
 			}
 			
 			?>
