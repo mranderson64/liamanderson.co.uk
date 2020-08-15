778838034b26e8297f82fc1a3d2a9100592322f7
mranderson64 <mranderson64@live.com>
2020-08-15 21:28:47 +0100

    Homepage slope and wide code

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,5 +1,5 @@
 <?php 
-$top = '<div class="container"><h1 class="tc grey">' . $contentTitle . '</h1>' . $xml->intro . '<div class="container"><div class="row">';
+$top = '<div class="container"><h1 class="tc grey">' . $contentTitle . '</h1>' . $xml->intro . '</div><div class="row">';
 $middle = '';
 $i = 1;
 while($i <= count($xml->panel)){
@@ -9,7 +9,9 @@ while($i <= count($xml->panel)){
 						<a href="' . $xml->panel[$o]->pURL . '">
 							<div class="project lighttealbg">
 								<div class="pimg darktealbg">
+									<svg preserveAspectRatio="none" viewBox="0 0 400 39" absolute-settings="bottom-0px-left-0px" style="transform: rotate(180deg);">  <path fill="#bee3de" class="cls-1" d="M400,39H0V38.3L400,0V39Z"></path></svg>
 									<img src="' . $host . '/IMAGES/' . $xml->panel[$o]->pImage . '" class="coverimg">
+									<svg preserveAspectRatio="none" viewBox="0 0 400 39" style="margin-bottom: -5px;">  <path fill="#bee3de" class="cls-1" d="M400,39H0V38.3L400,0V39Z"></path></svg>
 								</div>
 								<h3 class="navy">' . $xml->panel[$o]->pTitle . '</h3>
 								<p class="navy">' . $xml->panel[$o]->pDescription . '</p>
@@ -21,7 +23,9 @@ while($i <= count($xml->panel)){
 						<a href="' . $xml->panel[$o]->pURL . '">
 							<div class="project lighttealbg">
 								<div class="pimg darktealbg">
+									<svg preserveAspectRatio="none" viewBox="0 0 400 39" absolute-settings="bottom-0px-left-0px" style="transform: rotate(180deg);">  <path fill="#bee3de" class="cls-1" d="M400,39H0V38.3L400,0V39Z"></path></svg>
 									<img src="' . $host . '/IMAGES/' . $xml->panel[$o]->pImage . '" class="coverimg">
+									<svg preserveAspectRatio="none" viewBox="0 0 400 39" style="margin-bottom: -5px;">  <path fill="#bee3de" class="cls-1" d="M400,39H0V38.3L400,0V39Z"></path></svg>
 								</div>
 								<h3 class="navy">' . $xml->panel[$o]->pTitle . '</h3>
 								<p class="navy">' . $xml->panel[$o]->pDescription . '</p>
@@ -31,7 +35,9 @@ while($i <= count($xml->panel)){
 		$middle .= '<a href="' . $xml->panel[$o]->pURL . '">
 						<div class="project lighttealbg">
 							<div class="pimg darktealbg">
+								<svg preserveAspectRatio="none" viewBox="0 0 400 39" absolute-settings="bottom-0px-left-0px" style="transform: rotate(180deg);">  <path fill="#bee3de" class="cls-1" d="M400,39H0V38.3L400,0V39Z"></path></svg>
 								<img src="' . $host . '/IMAGES/' . $xml->panel[$o]->pImage . '" class="coverimg">
+								<svg preserveAspectRatio="none" viewBox="0 0 400 39" style="margin-bottom: -5px;">  <path fill="#bee3de" class="cls-1" d="M400,39H0V38.3L400,0V39Z"></path></svg>
 							</div>
 							<h3 class="navy">' . $xml->panel[$o]->pTitle . '</h3>
 							<p class="navy">' . $xml->panel[$o]->pDescription . '</p>
@@ -41,7 +47,7 @@ while($i <= count($xml->panel)){
 	} 
 	$i ++;			
 }
-$bottom = '</div></div></div>';
+$bottom = '</div></div>';
 
 $templateOutput = $top . $middle . $bottom;
 	
