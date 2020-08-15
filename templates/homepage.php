4b4d5f01e5570602adf368deacea52639006bccf
mranderson64 <mranderson64@live.com>
2020-08-15 21:53:55 +0100

    added slope classes to  dom

--- templates/homepage.php
+++ templates/homepage.php
@@ -9,9 +9,9 @@ while($i <= count($xml->panel)){
 						<a href="' . $xml->panel[$o]->pURL . '">
 							<div class="project lighttealbg">
 								<div class="pimg darktealbg">
-									<svg preserveAspectRatio="none" viewBox="0 0 400 39" style="transform: rotate(180deg);position: absolute;width: 99%;">  <path fill="#bee3de" class="cls-1" d="M400,39H0V38.3L400,0V39Z"></path></svg>
+									<svg preserveAspectRatio="none" viewBox="0 0 400 39" class="slopet">  <path fill="#bee3de" class="cls-1" d="M400,39H0V38.3L400,0V39Z"></path></svg>
 									<img src="' . $host . '/IMAGES/' . $xml->panel[$o]->pImage . '" class="coverimg">
-									<svg preserveAspectRatio="none" viewBox="0 0 400 39" style="position: relative;left: 0;bottom: 9.5vw;">  <path fill="#bee3de" class="cls-1" d="M400,39H0V38.3L400,0V39Z"></path></svg>
+									<svg preserveAspectRatio="none" viewBox="0 0 400 39" class="slopeb">  <path fill="#bee3de" class="cls-1" d="M400,39H0V38.3L400,0V39Z"></path></svg>
 								</div>
 								<h3 class="navy">' . $xml->panel[$o]->pTitle . '</h3>
 								<p class="navy">' . $xml->panel[$o]->pDescription . '</p>
@@ -23,9 +23,9 @@ while($i <= count($xml->panel)){
 						<a href="' . $xml->panel[$o]->pURL . '">
 							<div class="project lighttealbg">
 								<div class="pimg darktealbg">
-									<svg preserveAspectRatio="none" viewBox="0 0 400 39" style="transform: rotate(180deg);position: absolute;width: 99%;">  <path fill="#bee3de" class="cls-1" d="M400,39H0V38.3L400,0V39Z"></path></svg>
+									<svg preserveAspectRatio="none" viewBox="0 0 400 39" class="slopet">  <path fill="#bee3de" class="cls-1" d="M400,39H0V38.3L400,0V39Z"></path></svg>
 									<img src="' . $host . '/IMAGES/' . $xml->panel[$o]->pImage . '" class="coverimg">
-									<svg preserveAspectRatio="none" viewBox="0 0 400 39" style="position: relative;left: 0;bottom: 9.5vw;">  <path fill="#bee3de" class="cls-1" d="M400,39H0V38.3L400,0V39Z"></path></svg>
+									<svg preserveAspectRatio="none" viewBox="0 0 400 39" class="slopeb">  <path fill="#bee3de" class="cls-1" d="M400,39H0V38.3L400,0V39Z"></path></svg>
 								</div>
 								<h3 class="navy">' . $xml->panel[$o]->pTitle . '</h3>
 								<p class="navy">' . $xml->panel[$o]->pDescription . '</p>
@@ -35,9 +35,9 @@ while($i <= count($xml->panel)){
 		$middle .= '<a href="' . $xml->panel[$o]->pURL . '">
 						<div class="project lighttealbg">
 							<div class="pimg darktealbg">
-								<svg preserveAspectRatio="none" viewBox="0 0 400 39" style="transform: rotate(180deg);position: absolute;width: 99%;">  <path fill="#bee3de" class="cls-1" d="M400,39H0V38.3L400,0V39Z"></path></svg>
+								<svg preserveAspectRatio="none" viewBox="0 0 400 39" class="slopet">  <path fill="#bee3de" class="cls-1" d="M400,39H0V38.3L400,0V39Z"></path></svg>
 								<img src="' . $host . '/IMAGES/' . $xml->panel[$o]->pImage . '" class="coverimg">
-								<svg preserveAspectRatio="none" viewBox="0 0 400 39" style="position: relative;left: 0;bottom: 9.5vw;">  <path fill="#bee3de" class="cls-1" d="M400,39H0V38.3L400,0V39Z"></path></svg>
+								<svg preserveAspectRatio="none" viewBox="0 0 400 39" class="slopeb">  <path fill="#bee3de" class="cls-1" d="M400,39H0V38.3L400,0V39Z"></path></svg>
 							</div>
 							<h3 class="navy">' . $xml->panel[$o]->pTitle . '</h3>
 							<p class="navy">' . $xml->panel[$o]->pDescription . '</p>
