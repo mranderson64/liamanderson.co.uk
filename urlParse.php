f15e4f9b5395db571241bf32d7861e79d45b4594
MrAnderson64 <mranderson64@live.com>
2020-10-17 05:41:04 +0100

    accidentally removed urlParse module

---
+++ urlParse.php
@@ -0,0 +1,25 @@
+<?php
+$protocol = stripos($_SERVER['SERVER_PROTOCOL'],'https') === 0 ? 'https://' : 'http://';
+$urip = $_SERVER['REQUEST_URI'];
+$uri = str_replace("/", "-", $urip);
+#$urib = str_replace("/liam", "", $urip); // liams stupid dev enviroment
+#$uri = str_replace("/", "-", $urib);
+$host = $protocol . $_SERVER['HTTP_HOST'];
+if($uri == "" || $uri == "-" ){
+	$uri = "home";
+}
+$xml = simplexml_load_file("./XML/" . $uri . ".xml") or die("Error: Cannot create object");
+$title = $xml->title;
+$description = $xml->description;
+$useTemplate = $xml->useTemplate;
+$template = $xml->template;
+$usehtml = $xml->usehtml;
+$contentTitle = $xml->contentTitle;
+$content = $xml->content;
+if($useTemplate == "true"){
+	include "./templates/" . $template . ".php";
+	$parseOutput = $templateOutput;
+}else if($usehtml == "true"){
+	$parseOutput = '<div class="container">' . $content . '</div>';
+}
+?>
