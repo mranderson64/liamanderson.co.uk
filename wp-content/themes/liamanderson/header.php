cd096321400ff1fa65e4a77b9469e1b9b11269fc
MrAnderson64 <mranderson64@live.com>
2020-12-10 15:01:13 +0000

    Added Analytics javascript to head

--- wp-content/themes/liamanderson/header.php
+++ wp-content/themes/liamanderson/header.php
@@ -18,20 +18,28 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 
+		<!-- Google Tag Manager -->
+		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-KQCFLJ9');</script>
+		<!-- End Google Tag Manager -->
+
 		<link rel="profile" href="https://gmpg.org/xfn/11">
 
 		<?php wp_head(); ?>
 
 	</head>
 
-	
+
 	<body class="navybg <?php body_class(); ?>">
 	<?php
 		wp_body_open();
 		?>
 		<!-- Google Tag Manager (noscript) -->
 		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQCFLJ9" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-		<!-- End Google Tag Manager (noscript) -->	
+		<!-- End Google Tag Manager (noscript) -->
 			<div class="hero wA darktealbg">
 				<img src="https://blog.liamanderson.co.uk/wp-content/uploads/2020/07/hero-21.jpg" class="coverimg">
 				<div class="avi lighttealbg">
